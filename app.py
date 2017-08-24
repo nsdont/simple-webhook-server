ce8ae9d84762cb6a5737c6edf53cf4ceee434601
NSDont <haodewanan@gmail.com>
2017-08-24 15:22:20 +0800

    modify start option

--- app.py
+++ app.py
@@ -2,32 +2,37 @@
 
 import tornado.ioloop
 import tornado.web
+
 from datetime import datetime
+from tornado.options import define, options
+
+define("port", default=3000, help="run on the given port", type=int)
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self, *args, **kwargs):
         self.write('OK')
-        print("{} - {} - {} - {}".format(self.request.method,
-                                         self.request.uri,
-                                         self.request.body,
-                                         datetime.now()))
+        print("-------- request logger start ---------")
+        print(str(self.request.headers))
+        print(self.request.body)
+        print("-------- request logger end ---------")
 
     def post(self, *args, **kwargs):
         self.write('OK')
-        print("{} - {} - {} - {}".format(self.request.method,
-                                         self.request.uri,
-                                         self.request.body,
-                                         datetime.now()))
+        print("-------- request logger start ---------")
+        print(str(self.request.headers))
+        print(self.request.body)
+        print("-------- request logger end ---------")
 
 
 def make_app():
     return tornado.web.Application([
         (r"(.*)", MainHandler),
-    ], debug=True)
+    ])
 
 
 if __name__ == "__main__":
+    options.parse_command_line()
     app = make_app()
-    app.listen(3000)
+    app.listen(options.port)
     tornado.ioloop.IOLoop.current().start()
